b47e79697fe3da22cafe1296af82b7dfb627dda5
Jeyaprakashtech <Jeyaprakas2630@gmail.com>
2024-06-20 14:35:44 +0530

    small

--- my-portfolio2/app/page.js
+++ my-portfolio2/app/page.js
@@ -3,6 +3,7 @@
 
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import StarCanvas from "../app/components/canvas/StyledStarsCanvas";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
